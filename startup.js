ae888d3a590f4abd963f5393bea23f38cb46c0e9
citycide <thecitycide@gmail.com>
2017-02-13 01:49:16 -0600

    refactor: use logger in startup

--- startup.js
+++ startup.js
@@ -5,11 +5,12 @@ const { join } = require('path')
 const { readAsync } = require('fs-jetpack')
 
 const Core = require('./core')
+const log = require('./core/logger')
 const { paths } = require('./constants')
 
 
 module.exports = function startup (options) {
-  console.log('starting up...')
+  log.trace('starting up...')
   options.db = new Trilogy(join(paths.data, 'bot.db'))
   return readAsync(options.configPath, 'json')
     .then(config => new Core(config, options))
