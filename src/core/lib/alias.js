079902687395b291bd8f4b2450640f5c21a4abd2
Bo Lingen <thecitycide@gmail.com>
2017-03-20 18:57:56 -0500

    feat: implement command alias backend
    The alias will basically be a macro / shortcut, rather than a strict command <-> alias sharing of functionality. So given `!alias pa points add`, the following would be exactly the same:
    
    ```
    !points add citycide 50
    !pa citycide 50
    ```
    
    Trailing strings will remain as-is, which allows passing through arguments as if the original command was used normally.
    
    There's will be no difference in the event object, so modules won't be able to tell the difference between the alias being used vs the original command.

---
+++ src/core/lib/alias.js
@@ -0,0 +1,33 @@
+'use strict'
+
+module.exports = context => {
+  return db.model('alias', { name: String, original: String })
+    .then(() => {
+      function addAlias (name, original) {
+        let command = String(original).split(' ', 1)[0]
+        if (context.command.exists(name)) return false
+        if (!context.command.exists(command)) return false
+
+        return context.db.create('alias', { name, original })
+          .then(count => count === 1)
+      }
+
+      function removeAlias (name) {
+        return context.db.remove('alias', { name })
+          .then(count => count === 1)
+      }
+      
+      function isAlias (name) {
+        return context.db.findOne('alias', { name })
+          .then(alias => alias && alias.original)
+      }
+
+      context.extend({
+        command: {
+          addAlias
+          removeAlias,
+          isAlias
+        }
+      })
+    })
+}
